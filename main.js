5c436173c9b64e2f2347b63fe876f519ef0f967f
Kitti <kittitoth8@gmail.com>
2023-05-19 11:32:19 +0200

    add new cost form

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+function initializeOnWindowsLoad() {
+  getBalance();
+  setMonths();
+}
+
+const baseUrl = "https://berlo-egyenlege-backend.vercel.app";
 const url = "https://berlo-egyenlege-backend.vercel.app/balance";
 const requestParams = {
   headers: {
@@ -16,4 +22,47 @@ async function getBalance() {
       console.log("Something went wrong!");
       console.log(error);
     });
+}
+  
+  
+function setMonths() {
+  let dropDownMenu = document.getElementById("month-selection");
+  const months = {
+    "january": "január",
+    "february": "február",
+    "march": "március",
+    "april": "április",
+    "mai": "május",
+    "june": "június",
+    "july": "július",
+    "august": "augusztus",
+    "september": "szeptember",
+    "october": "október",
+    "november": "november",
+    "december": "december"
+  }
+
+  for (const month in months) {
+    const option = document.createElement("option");
+    option.value = month;
+    option.textContent = months[month];
+    dropDownMenu.appendChild(option);
+  }
+}
+
+
+function addCostFormSubmit(event, form) {
+  event.preventDefault();
+
+  const month = document.getElementById("month-selection").value;
+  const amount = document.getElementById("amount").value;
+
+  if(month && amount) {
+    // TODO: http request + handle response
+    //form.submit();
+    //fetch(baseUrl + form.action, {method:'post', body: new FormData(form)});
+
+  } else {
+    // TODO: error message for user
+  }
 }
