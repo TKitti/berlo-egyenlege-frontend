fdc5ea7f16e78c31b7cfb90ef92cea97e157d7e4
Kitti <kittitoth8@gmail.com>
2023-06-03 14:17:49 +0200

    add bootstrap modal for asking password when submutting form

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function initializeOnWindowsLoad() {
 }
 
 const baseUrl = "https://berlo-egyenlege-backend.vercel.app";
-const requestParams = {
+const requestParamsGetMethod = {
   headers: {
     "Content-Type": "application/json"
   }
 }
 
 async function getBalance() {
-  await fetch(`${baseUrl}/balance`, requestParams)
+  await fetch(`${baseUrl}/balance`, requestParamsGetMethod)
     .then(async response => {      
       const jsonData = await response.json();
       document.getElementById("balance").textContent = jsonData[0].balance + ",- Ft";
@@ -26,7 +26,7 @@ async function getBalance() {
 async function getCosts() {
   let table = document.getElementById("cost-table");
   
-  await fetch(`${baseUrl}/costs`, requestParams)
+  await fetch(`${baseUrl}/costs`, requestParamsGetMethod)
   .then(async response => {
     const jsonData = await response.json();
     
@@ -71,18 +71,18 @@ function formatAmount(amount) {
 function setMonths() {
   let dropDownMenu = document.getElementById("month-selection");
   const months = {
-    "january": "január",
-    "february": "február",
-    "march": "március",
-    "april": "április",
-    "mai": "május",
-    "june": "június",
-    "july": "július",
-    "august": "augusztus",
-    "september": "szeptember",
-    "october": "október",
-    "november": "november",
-    "december": "december"
+    "01": "január",
+    "02": "február",
+    "03": "március",
+    "04": "április",
+    "05": "május",
+    "06": "június",
+    "07": "július",
+    "08": "augusztus",
+    "09": "szeptember",
+    "10": "október",
+    "11": "november",
+    "12": "december"
   }
 
   for (const month in months) {
@@ -94,18 +94,87 @@ function setMonths() {
 }
 
 
-function addCostFormSubmit(event, form) {
+function addCostFormSubmit(event) {
   event.preventDefault();
+}
 
+function submitPassword() {
+  const passwordElements = document.getElementsByClassName("passwordField");
+  if (passwordElements && passwordElements.length == 1) {
+    const password = passwordElements[0].value;
+
+    if( modalTrigger == "cost") {
+      console.log("a create cost fog meghívódni");
+      createCost(password);
+    } else if (modalTrigger == "payment") {
+      console.log("a create payment fog meghívódni");
+      // TODO: add createPayment
+    }
+    
+  }
+ 
+}
+
+async function createCost(password) {
+  const year = document.getElementById("year-selection").value;
   const month = document.getElementById("month-selection").value;
   const amount = document.getElementById("amount").value;
+  
 
-  if(month && amount) {
-    // TODO: http request + handle response
-    //form.submit();
-    //fetch(baseUrl + form.action, {method:'post', body: new FormData(form)});
+  if (password && year && month && amount) {
+    /*
+    const data = {
+      "password": password,
+      "cost": {
+        "date": year + "-" + month,
+        "amount": amount
+      }
+    }
+    */
+    const data = {
+      "date": year + "-" + month,
+      "amount": amount
+    }
 
+    const requestParams = {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Basic " + btoa("password:" + password)
+      }, 
+      body: JSON.stringify(data)
+    }
+
+    console.log(data);
+
+    await fetch(`${baseUrl}/cost`, requestParams)
+    .then(async response => {
+      const jsonData = await response.json();
+  
+      if (jsonData) {}
+    })
+    .catch(error => {
+      console.log("can't create cost", error);
+    }); 
   } else {
+    console.log("password or amount or date is missing");
     // TODO: error message for user
   }
+
+}
+
+
+let modalTrigger = "";
+// get the triggering button of the modal
+let modalElement = document.getElementsByClassName("modal");
+if (modalElement) {
+  modalElement[0].addEventListener('show.bs.modal', (event) => {
+    const triggeringButton = event.relatedTarget;
+    const classAttributes = triggeringButton.getAttribute("class");
+    if (classAttributes.includes("cost")) {
+      modalTrigger = "cost";
+    } else if (classAttributes.includes("payment")) {
+      modalTrigger = "payment";
+    }
+  });
 }
